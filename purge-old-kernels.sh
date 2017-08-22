88c0ea37965747510ba8916eb056cae2c1f90030
Joel Mukuthu <joelmukuthu@gmail.com>
2017-08-22 23:22:33 +0200

    feat: add purge-old-kernels

---
+++ purge-old-kernels.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# source: http://stackoverflow.com/a/28020809/5852444
+function purge-old-kernels() {
+	echo $(dpkg --list | grep linux-image | awk '{ print $2 }' |  sort -V | sed -n '/'`uname -r`'/q;p') \
+		$(dpkg --list | grep linux-headers | awk '{ print $2 }' | sort -V | sed -n '/'"$(uname -r | sed "s/\([0-9.-]*\)-\([^0-9]\+\)/\1/")"'/q;p') | \
+		xargs sudo apt-get -y purge
+}
