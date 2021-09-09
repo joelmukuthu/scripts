e4bca1432639cb43a405f81ed60c1763566cdeb3
Joel Mukuthu <joelmukuthu@gmail.com>
2021-09-09 10:00:51 +0200

    feat: remove obsolete packages dependencies after purging old kernels

--- purge-old-kernels.sh
+++ purge-old-kernels.sh
@@ -5,4 +5,5 @@ function purge-old-kernels() {
 	echo $(dpkg --list | grep linux-image | awk '{ print $2 }' |  sort -V | sed -n '/'`uname -r`'/q;p') \
 		$(dpkg --list | grep linux-headers | awk '{ print $2 }' | sort -V | sed -n '/'"$(uname -r | sed "s/\([0-9.-]*\)-\([^0-9]\+\)/\1/")"'/q;p') | \
 		xargs sudo apt-get -y purge
+	sudo apt-get autoremove
 }
