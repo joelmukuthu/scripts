6bff270f5590991d69dd8993f13664223a7b854b
Joel Mukuthu <joelmukuthu@gmail.com>
2023-07-27 15:54:41 +0200

    fix: no need to checkout `master`

--- git-purge-local-branches.sh
+++ git-purge-local-branches.sh
@@ -2,8 +2,7 @@
 
 # source: http://stackoverflow.com/a/28020809/5852444
 function git-purge-local-branches() {
-	git checkout master; \
-		git  pull --rebase; \
+	git  pull --rebase; \
 		git  fetch --all -p; \
 		git branch -vv | \
 		grep ": gone]" | \
